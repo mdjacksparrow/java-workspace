19ee6f6036b7d329f7f50824d459dc610a973a20
mdjacksparrow <45224729+mdjacksparrow@users.noreply.github.com>
2020-02-17 12:17:07 +0530

    Add files via upload

---
+++ Sparrow.java
@@ -0,0 +1,10 @@
+
+package sparrow;
+  
+public class Sparrow {
+   public static void main(String[] args)throws Exception {
+       ExcelOp obj = new ExcelOp();
+       
+       obj.readSheet();
+   }
+}
